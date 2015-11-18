00b87b12327df41ab3705a2d1c42fe7a02b6ccf9
Jean Parpaillon <jean.parpaillon@free.fr>
2015-11-18 19:33:40 +0100

    Fix indent

--- src/occi_category_mgr.erl
+++ src/occi_category_mgr.erl
@@ -27,15 +27,15 @@
 
 %% API
 -export([init/0,
-	 hash/1]).
+         hash/1]).
 -export([get/1,
-	 find/1,
-	 find_all/0,
-	 load_schema/2,
-	 register_kind/1,
-	 register_mixin/1,
-	 register_action/1,
-	 unregister_mixin/1]).
+         find/1,
+         find_all/0,
+         load_schema/2,
+         register_kind/1,
+         register_mixin/1,
+         register_action/1,
+         unregister_mixin/1]).
 
 -define(CAT_TBL, ?MODULE).
 
@@ -46,21 +46,21 @@
 init() ->
     ?info("Starting OCCI categories manager"),
     ?CAT_TBL = ets:new(?CAT_TBL, 
-		       [ordered_set, public, {keypos, 2}, named_table, {read_concurrency, true}]),
+                       [ordered_set, public, {keypos, 2}, named_table, {read_concurrency, true}]),
     ok.
 
 -spec load_schema(Backend :: atom(), Schema :: occi_schema()) -> ok | {error, term()}.
 load_schema(_Backend, Ext) ->
     case get_extension(Ext) of
-	{error, parse_error} ->
-	    {error, parse_error};
-	#occi_extension{}=E ->
-	    lists:foreach(fun(#occi_kind{id=Id}=Kind) ->
-				  register_kind(Kind#occi_kind{location=get_uri(Id)});
-			     (#occi_mixin{id=Id}=Mixin) ->
-				  register_mixin(Mixin#occi_mixin{location=get_uri(Id)})
-			  end,
-			  occi_extension:get_categories(E))
+        {error, parse_error} ->
+            {error, parse_error};
+        #occi_extension{}=E ->
+            lists:foreach(fun(#occi_kind{id=Id}=Kind) ->
+                                  register_kind(Kind#occi_kind{location=get_uri(Id)});
+                             (#occi_mixin{id=Id}=Mixin) ->
+                                  register_mixin(Mixin#occi_mixin{location=get_uri(Id)})
+                          end,
+                          occi_extension:get_categories(E))
     end.
 
 
@@ -68,17 +68,17 @@ register_kind(#occi_kind{id=Id, location=#uri{}=Uri}=Kind) ->
     ?info("Registering kind: ~p -> ~p~n", [ Id, Uri ]),
     ets:insert(?CAT_TBL, Kind),
     lists:foreach(fun(Action) ->
-			  register_action(Action)
-		  end,
-		  occi_kind:get_actions(Kind)).
+                          register_action(Action)
+                  end,
+                  occi_kind:get_actions(Kind)).
 
 register_mixin(#occi_mixin{id=Id, location=Uri}=Mixin) ->
     ?info("Registering mixin: ~p -> ~p~n", [ Id, Uri ]),
     ets:insert(?CAT_TBL, Mixin),
     lists:foreach(fun(Action) ->
-			  register_action(Action)
-		  end,
-		  occi_mixin:get_actions(Mixin)).
+                          register_action(Action)
+                  end,
+                  occi_mixin:get_actions(Mixin)).
 
 unregister_mixin(#occi_mixin{id=Id, location=Uri}) ->
     ?info("Unregistering mixin: ~p -> ~p~n", [ Id, Uri ]),
@@ -91,21 +91,21 @@ register_action(#occi_action{id=Id}=Action) ->
 -spec get(occi_cid()) -> {ok, occi_category()} | {error, term()}.
 get(#occi_cid{}=Cid) ->
     case find(Cid) of
-	[] ->
-	    {error, unknown_cid};
-	[Res] ->
-	    {ok, Res};
-	_ ->
-	    {error, invalid_cid}
+        [] ->
+            {error, unknown_cid};
+        [Res] ->
+            {ok, Res};
+        _ ->
+            {error, invalid_cid}
     end.
 
 -spec find(occi_category() | uri()) -> [occi_category()].
 find(#uri{path=Path}) ->
     case ets:match_object(?CAT_TBL, #occi_kind{location=#uri{path=Path, _='_'}, _='_'}) of
-	[] ->
-	    ets:match_object(?CAT_TBL, #occi_mixin{location=#uri{path=Path, _='_'}, _='_'});
-	Other ->
-	    Other
+        [] ->
+            ets:match_object(?CAT_TBL, #occi_mixin{location=#uri{path=Path, _='_'}, _='_'});
+        Other ->
+            Other
     end;
 
 find(#occi_cid{class=kind}=Cid) ->
@@ -119,10 +119,10 @@ find(#occi_cid{class=mixin}=Cid) ->
 
 find(#occi_cid{class='_'}=Cid) ->
     case ets:match_object(?CAT_TBL, #occi_kind{id=Cid#occi_cid{class=kind}, _='_'}) of
-	[] ->
-	    ets:match_object(?CAT_TBL, #occi_mixin{id=Cid#occi_cid{class=mixin}, _='_'});
-	Res ->
-	    Res
+        [] ->
+            ets:match_object(?CAT_TBL, #occi_mixin{id=Cid#occi_cid{class=mixin}, _='_'});
+        Res ->
+            Res
     end;
 
 find(#occi_kind{}=Kind) ->
@@ -159,18 +159,18 @@ get_uri(Id) ->
 hash1(#occi_cid{term=Term}=Cid, Prefix, 0) ->
     U = #uri{path=lists:flatten(io_lib:format("~s/~s/", [Prefix, term_to_list(Term)]))},
     case find(U) of
-	[] -> U;
-	_ ->
-	    % Conflict !
-	    hash1(Cid, Prefix, 1)
+        [] -> U;
+        _ ->
+                                                % Conflict !
+            hash1(Cid, Prefix, 1)
     end;
 hash1(#occi_cid{term=Term}=Cid, Prefix, I) ->
     U = #uri{path=lists:flatten(io_lib:format("~s/~s~.b/", [Prefix, term_to_list(Term), I]))},
     case find(U) of
-	[] -> U;
-	_ ->
-	    % Conflict !
-	    hash1(Cid, Prefix, I+1)
+        [] -> U;
+        _ ->
+                                                % Conflict !
+            hash1(Cid, Prefix, I+1)
     end.    
 
 term_to_list(Term) when is_atom(Term) -> atom_to_list(Term);
