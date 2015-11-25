0dba770bce9bc0e4d5a4d223b812ebc15a54f6d8
Jean Parpaillon <jean.parpaillon@free.fr>
2015-11-25 14:22:20 +0100

    Fix indent

--- src/occi_parser_text.hrl
+++ src/occi_parser_text.hrl
@@ -2,10 +2,10 @@
 
 -type(req_type() :: action | entity | user_mixin | collection).
 -record(state, {type                                :: req_type(),
-		collection                          :: occi_collection(),
-		entity         = undefined          :: term(),
-		entity_id      = undefined          :: uri(),
-		mixin          = undefined          :: term(),
-		action         = undefined          :: occi_action()}).
+                collection                          :: occi_collection(),
+                entity         = undefined          :: term(),
+                entity_id      = undefined          :: uri(),
+                mixin          = undefined          :: term(),
+                action         = undefined          :: occi_action()}).
 
 -define(hdr_to_atom(X), occi_known:hdr_to_atom(X)).
