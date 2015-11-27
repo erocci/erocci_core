b716491db713a95d7c56c8a22cc2ae66ab888c21
Jean Parpaillon <jean.parpaillon@free.fr>
2015-11-27 16:01:31 +0100

    Fix indent

--- src/occi_renderer_occi.erl
+++ src/occi_renderer_occi.erl
@@ -29,7 +29,7 @@
 
 %% API
 -export([render/2,
-	 render_headers/2]).
+         render_headers/2]).
 
 %%%===================================================================
 %%% API
@@ -39,9 +39,9 @@ render(Node, Env) ->
 
 render_headers(Headers, #occi_env{req=Req}=Env) ->
     Req2 = lists:foldl(fun (Name, Acc) -> 
-			       Value = occi_renderer:join(
-					 lists:reverse(orddict:fetch(Name, Headers)),
-					 ", "),
-			       cowboy_req:set_resp_header(Name, Value, Acc)
-		       end, Req, orddict:fetch_keys(Headers)),
+                               Value = occi_renderer:join(
+                                         lists:reverse(orddict:fetch(Name, Headers)),
+                                         ", "),
+                               cowboy_req:set_resp_header(Name, Value, Acc)
+                       end, Req, orddict:fetch_keys(Headers)),
     {<<"OK\n">>, Env#occi_env{req=Req2}}.
