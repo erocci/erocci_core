bf501ddbe54bd724ed5d839ade56d073af49ec5e
Jean Parpaillon <jean.parpaillon@free.fr>
2016-05-19 16:09:14 +0200

    Start pretty printing errors

--- src/erocci_errors.erl
+++ src/erocci_errors.erl
@@ -7,6 +7,8 @@
 
 -module(erocci_errors).
 
+-include("erocci_log.hrl").
+
 -export([render/2]).
 
 -type t() :: erocci_store:error()
@@ -16,5 +18,43 @@
 %% @doc Render errors
 %% @end
 -spec render(occi_utils:mimetype(), t()) -> binary().
-render(_Mimetype, _Errors) ->
+render(Mimetype, Errors) ->
+    ?debug("ERROR (~p): ~p", [Mimetype, Errors]),
+    (renderer(occi_utils:normalize_mimetype(Mimetype)))(Errors).
+
+
+%%%
+%%% Priv
+%%%
+renderer({<<"application">>, <<"occi+xml">>, _}) -> fun render_xml/1;
+renderer({<<"application">>, <<"occi+json">>, _})-> fun render_json/1;
+renderer({<<"text">>, <<"occi+plain">>, _})      -> fun render_plain/1;
+renderer({<<"text">>, <<"occi">>, _})            -> fun render_occi/1;
+renderer({<<"text">>, <<"uri-list">>, _})        -> fun render_uri/1.
+
+
+render_xml(_Errors) ->
+    <<>>.
+
+
+render_json(_Errors) ->
+    <<>>.
+
+
+render_plain(Errors) ->
+    render_plain(Errors, []).
+
+
+render_plain([], Acc) ->
+    Acc;
+
+render_plain([ Error | Tail ], Acc) ->
+    render_plain(Tail, [ Acc, io_lib:format("error: ~p~n", [Error]) ]).
+
+
+render_occi(_Errors) ->
+    <<>>.
+
+
+render_uri(_Errors) ->
     <<>>.
